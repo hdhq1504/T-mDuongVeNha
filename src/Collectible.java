f69a380f74a1666341143f7cccfec71da8215ab8
hdhq1504 <hoquan15042004@gmail.com>
2025-05-22 01:02:10 +0700

    add Collectible.java

---
+++ src/Collectible.java
@@ -0,0 +1,43 @@
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 11a5h
+ */
+public class Collectible {
+    private int row, col;
+    private Image image;
+    private int points;
+    private int score = 0;
+    private boolean collected = false;
+
+    public Collectible(int row, int col, Image image, int points) {
+        this.row = row;
+        this.col = col;
+        this.image = image;
+        this.points = points;
+    }
+
+    public boolean isAt(int r, int c) {
+        return row == r && col == c && !collected;
+    }
+
+    public void collect() {
+        collected = true;
+        score += points;
+        // updateStatusLabel();
+    }
+
+    public void draw(Graphics g, int cellSize) {
+        if (!collected) {
+            g.drawImage(image, col * cellSize, row * cellSize, cellSize, cellSize, null);
+        }
+    }
+}
